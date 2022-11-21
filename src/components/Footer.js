b64e47562e16c747ee4900e167a5411907ede4c7
Emily LaBelle <emilylabelle8@gmail.com>
2022-11-21 13:54:30 -0500

    add posts and craeted home page

---
+++ src/components/Footer.js
@@ -0,0 +1,5 @@
+export default function Footer() {
+  return (
+    <footer><small>Copyright &copy; 2022 | all rights reserved </small></footer>
+  )
+}
---
+++ src/pages/AddPost.js
@@ -0,0 +1,23 @@
+import { useState } from "react"
+
+export default function AddPost() {
+  const [form, setForm] = useState({})
+
+  const handleForm = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
+  return (
+    <div className="container">
+      <h1>Add Post</h1>
+      <form action="">
+        <label htmlFor="">Author</label>
+        <input name='author' type="text" onChange={handleForm}/>
+        <label htmlFor="">Text</label>
+        <input name='text' type="text" onChange={handleForm}/>
+        <label htmlFor="">Date</label>
+        <input name='date' type="date" onChange={handleForm}/>
+      </form>
+    </div>
+  )
+}
